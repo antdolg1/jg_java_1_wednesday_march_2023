b1f93d05fcd7eb41eef59427a4db3dc34ca7982a
Antons Dolgopolovs <antdolg@gmail.com>
2023-03-29 21:47:22 +0300

    Add lesson_2 lessoncode

---
+++ src/main/java/teacher/lesson_2/lessoncode/ArithmeticOperators.java
@@ -0,0 +1,23 @@
+package teacher.lesson_2.lessoncode;
+
+public class ArithmeticOperators {
+
+    public static void main(String[] args) {
+
+        int number1 = 10;
+        int number2 = 4;
+
+        double number3 = 10.0;
+        double number4 = 4.0;
+
+        int sum = number1 + number2;
+        int division = number1 / number2;
+        double divisionOfDoubles = number3 / number4;
+
+        System.out.println("Sum of number1 and number2 is " + sum);
+
+        System.out.println("Division of 10 to 4 result is " + division);
+        System.out.println("Division of 10 to 4 result is " + divisionOfDoubles);
+
+    }
+}
