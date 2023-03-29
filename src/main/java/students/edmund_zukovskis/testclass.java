9a3d17d83aeaab7893ee634929440d6f206092e5
Edmundzuk <edmundzukovskis@gmail.com>
2023-03-29 22:14:54 +0300

    Lesson 2 tested how int and sum works

---
+++ src/main/java/students/edmund_zukovskis/testclass.java
@@ -0,0 +1,16 @@
+package students.edmund_zukovskis;
+
+import java.sql.SQLOutput;
+
+public class testclass {
+
+    public static void main(String[] args) {
+
+        int number1 = 15;
+        int number2 = 12;
+
+        int sum = number1 + number2;
+
+        System.out.println("Sum of number1 and number2 is " + sum);
+    }
+}
