0dbca92bcb286b00256254c4490c0b3fdae79333
pavel <pavels.serikovs@gmail.com>
2023-04-01 22:23:10 +0300

    Hello world!

---
+++ src/main/java/students/pavels_serikovs/HelloWrld.java
@@ -0,0 +1,11 @@
+package students.pavels_serikovs;
+
+public class HelloWrld {
+    public static void main(String[] args) {
+        int a = 22;
+        int b = 12;
+        int c = (a+b)*2;
+        System.out.println("Hello World!");
+        System.out.println("The result is " +c);
+    }
+}
