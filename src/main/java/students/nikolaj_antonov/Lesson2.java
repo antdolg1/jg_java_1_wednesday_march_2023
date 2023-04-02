32c7d8f5e8a1641565a3b2ee28661ba16de5cd9a
Nik737 <nikolaj.antonov@gmail.com>
2023-04-02 22:31:57 +0300

    First Hello

---
+++ src/main/java/students/nikolaj_antonov/Lesson2.java
@@ -0,0 +1,14 @@
+package students.nikolaj_antonov;
+
+public class Lesson2 {
+
+    public static void main(String[] args) {
+
+        int a = 11;
+        int b = 22;
+
+        int sum = a + b;
+
+        System.out.println("Sum of a and b is" + sum);
+    }
+}
