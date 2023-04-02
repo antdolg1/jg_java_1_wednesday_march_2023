f0b0c08f79b76e0d281b114139a318260deceaae
SlavaJavaGuru <slv53@inbox.lv>
2023-04-02 20:45:51 +0300

    That is my first file in this practice.

---
+++ src/main/java/students/slava_khl/Task_1.java
@@ -0,0 +1,27 @@
+package students.slava_khl;
+
+public class Task_1 {
+
+    public static void main(String[] args) {
+
+        int number1 = 22;
+        int number2 = 44;
+
+        int result_sum = number1 + number2;
+
+        int result_sub = number2 - number1;
+
+        int result_mul = number1 * number2;
+
+        int result_div = number2 / number1;
+
+
+        System.out.println("Task_1_Slava_khl");
+        System.out.println("Sum of number1 and number2 is " + result_sum);
+        System.out.println("Sub of number2 and number1 is " + result_sub);
+        System.out.println("Mul of number1 and number2 is " + result_mul);
+        System.out.println("Division of number2 to number1 is " + result_div);
+
+    }
+
+}
