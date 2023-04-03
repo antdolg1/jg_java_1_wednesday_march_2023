54d4fa90dac7b7426f096148a19c1e6aa50f0827
AndrejsMihailicenko <andrejsmihailicenko@gmail.com>
2023-04-03 12:26:17 +0300

    Helloapp commit

--- src/main/java/students/andrej_mihailicenko/Helloapp.java
+++ src/main/java/students/andrej_mihailicenko/Helloapp.java
@@ -1,4 +1,8 @@
 package students.andrej_mihailicenko;
 
 public class Helloapp {
+    public static void main(String[] args) {
+        String x = "Hello Andrey!";
+        System.out.println(x);
+    }
 }
