fbc1db3ca02c0dd5ed9bea6afe8cfa57873169d7
Nik737 <nikolaj.antonov@gmail.com>
2023-04-03 22:11:19 +0300

    Results of...

--- src/main/java/students/nikolaj_antonov/Lesson2.java
+++ src/main/java/students/nikolaj_antonov/Lesson2.java
@@ -8,15 +8,30 @@ public class Lesson2 {
         int B = 20;
         int C = 30;
 
+        float D = 1.1f;
+        float F = 2.0f;
+
+        double G = 4.0;
+
         int sum = A + B + C;
         int sub = C - B;
         int div = C / A;
         int mul = A * B * C;
 
-        System.out.println("Result of A and B and C is " + sum);
-        System.out.println("Result of C and B is " + sub);
-        System.out.println("Result of C and A is " + div);
-        System.out.println("Result of A and B and C is " + mul);
+        float Div = D / F;
+
+        double DIV = G / B;
+
+
+        System.out.println("Result of sum A and B and C is " + sum);
+        System.out.println("Result of sub C and B is " + sub);
+        System.out.println("Result of div C and A is " + div);
+        System.out.println("Result of mul A and B and C is " + mul);
+
+        System.out.println("Result of Div D and F is " + Div);
+
+        System.out.println("RESULT is " + DIV);
+
 
 
     }
