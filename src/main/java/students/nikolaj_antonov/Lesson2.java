552e0c2041987d67f11b9a1605059e02daf94d3d
nikolajantonov <129219553+nikolajantonov@users.noreply.github.com>
2023-04-03 20:07:46 +0300

    Add files via upload

--- src/main/java/students/nikolaj_antonov/Lesson2.java
+++ src/main/java/students/nikolaj_antonov/Lesson2.java
@@ -13,10 +13,11 @@ public class Lesson2 {
         int div = C / A;
         int mul = A * B * C;
 
-        System.out.println("Result of A and B and C is" + sum);
-        System.out.println("Result of C and B is" + sub);
-        System.out.println("Result of C and A is" + div);
-        System.out.println("Result of A and B and C is" + mul);
+        System.out.println("Result of A and B and C is " + sum);
+        System.out.println("Result of C and B is " + sub);
+        System.out.println("Result of C and A is " + div);
+        System.out.println("Result of A and B and C is " + mul);
+
 
     }
 }
