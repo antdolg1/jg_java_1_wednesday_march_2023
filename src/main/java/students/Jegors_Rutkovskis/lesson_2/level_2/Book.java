5e36c43147e044e83b9695079b16329dadba0bc3
Jegors Rutkovskis <domerungus@gmail.com>
2023-04-04 19:07:21 +0300

    added lesson_2 homework

--- src/main/java/students/Jegors_Rutkovskis/lesson_2/level_2/Book.java
+++ src/main/java/students/Jegors_Rutkovskis/lesson_2/level_2/Book.java
@@ -2,7 +2,7 @@ package students.Jegors_Rutkovskis.lesson_2.level_2;
 
 class Book {
     public static void main(String[] args) {
-        
+
     }
 
 }
---
+++ src/main/java/students/Jegors_Rutkovskis/lesson_2/level_3/Average.java
@@ -0,0 +1,19 @@
+package students.Jegors_Rutkovskis.lesson_2.level_3;
+
+import java.util.Scanner;
+
+class Average {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int first = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int second = scanner.nextInt();
+        System.out.print("Enter third number: ");
+        int third = scanner.nextInt();
+
+        double average = (first + second + third) / 3;
+
+        System.out.println("Average is = " + average);
+    }
+}
