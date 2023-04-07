8f183cca100fd8dcedeadeaa950350b1ca316b12
Edmundzuk <edmundzukovskis@gmail.com>
2023-04-07 12:04:32 +0300

    Lesson 2 testing key binds

---
+++ src/main/java/students/edmund_zukovskis/lesson_2/level_5/Print.java
@@ -0,0 +1,11 @@
+package students.edmund_zukovskis.lesson_2.level_5;
+
+class Print {
+    public static void main(String[] args) {
+        System.out.println("Text in ");
+        System.out.println("Two lines ");
+        System.out.println("Text out ");
+        System.out.println("One line ");
+
+    }
+}
