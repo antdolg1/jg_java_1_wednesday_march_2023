72ebe4a6768b3dde3d171efc7b7d8fb94874e3ac
AndrejsMihailicenko <andrejsmihailicenko@gmail.com>
2023-04-07 12:10:06 +0300

    Add lesson_2 level_1 homeworks

---
+++ src/main/java/students/andrej_mihailicenko/lesson_2/level_1/CalculatorApp.java
@@ -0,0 +1,27 @@
+package students.andrej_mihailicenko.lesson_2.level_1;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class CalculatorApp {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int number1 = scanner.nextInt();
+        System.out.println("Enter second number: ");
+        int number2 = scanner.nextInt();
+        System.out.println("Results: ");
+
+        int summation = number1 + number2;
+        System.out.println(number1 + "+" + number2 + "=" + summation);
+
+        int subtraction = number1 - number2;
+        System.out.println(number1 + "-" + number2 + "=" + subtraction);
+
+        int multiplication = number1 * number2;
+        System.out.println(number1 + "*" + number2 + "=" + multiplication);
+
+        int division = number1 / number2;
+        System.out.println(number1 + "/" + number2 + "=" + division);
+
+    }
+}
