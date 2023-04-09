a06b8c285e679d4082e59d78b86e67fbb23d70b4
SlavaJavaGuru <slv53@inbox.lv>
2023-04-09 11:46:16 +0300

    Corrected homework's files...

--- src/main/java/students/slava_khl/lesson_2/level_1/DoubleMathActions.java
+++ src/main/java/students/slava_khl/lesson_2/level_1/DoubleMathActions.java
@@ -1,28 +1,20 @@
 package students.slava_khl.lesson_2.level_1;
-
 import java.util.Scanner;
-
-public class intMathActions {
-
+public class DoubleMathActions {
     public static void main(String[] args) {
-
-        System.out.println("Enter first num: ");
+        System.out.println("Enter first num:");
         Scanner scanner = new Scanner(System.in);
-        int firstNum = scanner.nextInt();
-        System.out.println("Enter second num: ");
-        int secondNum = scanner.nextInt();
-        int addition = firstNum + secondNum;
-        int substraction = firstNum - secondNum;
-        int multiplication = firstNum * secondNum;
-        int division = firstNum / secondNum;
-        System.out.println("Actions: ");
+        double firstNum = scanner.nextDouble();
+        System.out.print("Enter second num:");
+        double secondNum = scanner.nextDouble();
+        double addition = firstNum + secondNum;
+        double substraction = firstNum - secondNum;
+        double multiplication = firstNum * secondNum;
+        double division = firstNum / secondNum;
+        System.out.println("Actions:");
         System.out.println(firstNum + "+" + secondNum + "=" + addition);
         System.out.println(firstNum + "-" + secondNum + "= " + substraction);
         System.out.println(firstNum + "*" + secondNum + "= " + multiplication);
         System.out.println(firstNum + ":" + secondNum + "= " + division);
-
-
     }
-
-
 }
