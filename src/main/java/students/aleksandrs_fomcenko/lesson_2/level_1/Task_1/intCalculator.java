82df85cf3f907badcac87ec630b6898ecd87797e
afomcenko <aleksandrs.fomcenko@gmail.com>
2023-04-10 23:36:47 +0300

    Add Lesson 2, Level 1 homework Task 1

---
+++ src/main/java/students/aleksandrs_fomcenko/lesson_2/level_1/Task_1/intCalculator.java
@@ -0,0 +1,22 @@
+package students.aleksandrs_fomcenko.lesson_2.level_1.Task_1;
+import  java.util.Scanner;
+public class intCalculator {
+    public static void main(String[] args)  {
+        System.out.println("Write first number" );
+        Scanner scanner = new Scanner(System.in);
+        int firstNumber = scanner.nextInt();
+        System.out.println("Write second number" );
+        int secondNumber = scanner.nextInt();
+
+        int sum = firstNumber + secondNumber;
+        int subtraction = firstNumber - secondNumber;
+        int multiplication = firstNumber * secondNumber;
+        int divide = firstNumber / secondNumber;
+
+        System.out.println("sum = " + sum);
+        System.out.println("subtraction = " + subtraction);
+        System.out.println("multiplication = " + multiplication);
+        System.out.println("divide = " + divide);
+    }
+
+}
