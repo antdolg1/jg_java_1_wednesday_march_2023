87738e56c5d8756b673b338b65281633220b203a
Edmundzuk <edmundzukovskis@gmail.com>
2023-04-10 16:39:20 +0300

    Lesson 3 added homework level 1 - 2 - 3

---
+++ src/main/java/students/edmund_zukovskis/lesson_3/level_2/StringObject.java
@@ -0,0 +1,24 @@
+package students.edmund_zukovskis.lesson_3.level_2;
+
+public class StringObject {
+    public static void main(String[] args) {
+        String s1 = "Edmunds";
+        String s2 = "Edmunds";
+        String s3 = new String("Edmunds");
+
+        boolean nameMatch = s1 == s2;
+        System.out.println(nameMatch);
+
+        boolean nameDoNotMatch = s1 == s3;
+        System.out.println(nameDoNotMatch);
+
+        boolean nameMatch2 = s1.equals(s3);
+        System.out.println(nameMatch2);
+
+
+
+
+
+
+    }
+}
