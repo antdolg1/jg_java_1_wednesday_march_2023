730546a976d55d6c715916df13ea69ec34fdfff2
Nik737 <nikolaj.antonov@gmail.com>
2023-04-10 22:32:12 +0300

    Results of...

--- src/main/java/students/nikolaj_antonov/lesson_2/level_2/task_7/LoginService.java
+++ src/main/java/students/nikolaj_antonov/lesson_2/level_2/task_7/LoginService.java
@@ -1,6 +1,6 @@
 package students.nikolaj_antonov.lesson_2.level_2.task_7;
 
-class  LoginService {
+class LoginService {
 
     public static void main(String[] args) {
         System.out.println("You can login!");
