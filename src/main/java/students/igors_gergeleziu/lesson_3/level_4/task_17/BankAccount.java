61881689d66c813c0870293b24a3078fd4a222bd
MrJohnyRony <disqueeze@mail.ru>
2023-04-12 17:51:55 +0300

    Tasks lesson_3

---
+++ src/main/java/students/igors_gergeleziu/lesson_3/level_4/task_17/BankAccount.java
@@ -0,0 +1,28 @@
+package students.igors_gergeleziu.lesson_3.level_4.task_17;
+
+class BankAccount {
+    String ownerFirstName;
+    String ownerLastName;
+    int moneyAmount;
+
+   public BankAccount(String ownerFirstName,
+                String ownerLastName,
+                int moneyAmount) {
+        this.ownerFirstName = ownerFirstName;
+        this.ownerLastName = ownerLastName;
+        this.moneyAmount = moneyAmount;
+    }
+
+    String getOwnerFirstName() {
+        return this.ownerFirstName;
+    }
+
+    String getOwnerLastName() {
+        return this.ownerLastName;
+    }
+
+    int getMoneyAmount() {
+        return this.moneyAmount;
+    }
+
+}
