c42370cd2ecf1c08f47e5cc8981c78b1b9478e79
Nik737 <nikolaj.antonov@gmail.com>
2023-04-17 00:40:28 +0300

    Lesson 3 homeworks

---
+++ src/main/java/students/nikolaj/antonov/lesson_3/level_2/Task_7/Stringg.java
@@ -0,0 +1,10 @@
+package students.nikolaj.antonov.lesson_3.level_2.Task_7;
+
+public class Stringg {
+
+    public static void main(String[] args) {
+        String myName = "Nikolaj";
+        System.out.println(myName);
+    }
+
+}
