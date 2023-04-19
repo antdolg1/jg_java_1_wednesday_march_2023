1f646330fec89753b9557e1bde475c3080054eb2
Nik737 <nikolaj.antonov@gmail.com>
2023-04-19 13:22:01 +0300

    Lesson 3 homeworks

---
+++ src/main/java/students/nikolaj/antonov/lesson_3/level_4/Task_16/Car.java
@@ -0,0 +1,9 @@
+package students.nikolaj.antonov.lesson_3.level_4.Task_16;
+
+class Car {
+
+    String model;
+    Car(String newModel) {this.model = newModel;}
+    String getModel (){return this.model;}
+
+}
