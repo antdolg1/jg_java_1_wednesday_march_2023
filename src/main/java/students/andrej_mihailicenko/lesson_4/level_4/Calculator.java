3f078263d648a5bc412900d2da40fd7b10126dd4
AndrejsMihailicenko <andrejsmihailicenko@gmail.com>
2023-04-20 22:06:21 +0300

    Lesson 4 add level_4 tasks

---
+++ src/main/java/students/andrej_mihailicenko/lesson_4/level_4/Calculator.java
@@ -0,0 +1,47 @@
+package students.andrej_mihailicenko.lesson_4.level_4;
+
+class Calculator {
+    public int sum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public int subtract(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public int multiply(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    public int division(int firstNumber, int secondNumber) {
+        return firstNumber / secondNumber;
+    }
+
+    public boolean isFirstEven(int firstNumber) {
+        return firstNumber % 2 == 0;
+    }
+
+    public boolean isSecondEven(int secondNumber) {
+        return secondNumber % 2 == 0;
+    }
+
+    public int maxOfTwoNumbers(int firstNumber, int secondNumber) {
+        if (firstNumber > secondNumber) {
+            return firstNumber;
+        } else if (firstNumber < secondNumber) {
+            return secondNumber;
+        } else {
+            return firstNumber = secondNumber;
+        }
+    }
+
+    public int maxOfThreeNumbers(int firstNumber, int secondNumber, int thirdNumber) {
+        if (firstNumber >= secondNumber && firstNumber >= thirdNumber) {
+            return firstNumber;
+        } else if (firstNumber <= secondNumber && secondNumber >= thirdNumber) {
+            return secondNumber;
+        } else {
+            return thirdNumber;
+        }
+    }
+}
---
+++ src/main/java/students/andrej_mihailicenko/lesson_4/level_4/CalculatorTest.java
@@ -0,0 +1,223 @@
+package students.andrej_mihailicenko.lesson_4.level_4;
+
+class CalculatorTest {
+    public static void main(String[] args) {
+        CalculatorTest calculatorTest = new CalculatorTest();
+        calculatorTest.sumTest();
+        calculatorTest.subtractTest();
+        calculatorTest.multiplyTest();
+        calculatorTest.divisionTest();
+        calculatorTest.isFirstEvenTest();
+        calculatorTest.isSecondEvenTest();
+        calculatorTest.firstMaxOfTwoNumbersTest();
+        calculatorTest.secondMaxOfTwoNumbersTest();
+        calculatorTest.thirdMaxOfTwoNumbersTest();
+        calculatorTest.firstMaxOfThreeNumbersTest();
+        calculatorTest.secondMaxOfThreeNumbersTest();
+        calculatorTest.thirdMaxOfThreeNumbersTest();
+        calculatorTest.fourthMaxOfThreeNumbersTest();
+        calculatorTest.fifthMaxOfThreeNumbersTest();
+        calculatorTest.sixthMaxOfThreeNumbersTest();
+    }
+
+
+    public void sumTest() {
+        int firstNumber = 10;
+        int secondNumber = 5;
+        int expectedResult = 15;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.sum(firstNumber, secondNumber);
+        if (realResult == expectedResult) {
+            System.out.println("Sum test = OK");
+        } else {
+            System.out.println("Sum test = FAIL");
+        }
+    }
+
+    public void subtractTest() {
+        int firstNumber = 10;
+        int secondNumber = 5;
+        int expectedResult = 5;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.subtract(firstNumber, secondNumber);
+        if (realResult == expectedResult) {
+            System.out.println("Subtract test = OK");
+        } else {
+            System.out.println("Subtract test = FAIL");
+        }
+    }
+
+    public void multiplyTest() {
+        int firstNumber = 10;
+        int secondNumber = 5;
+        int expectedResult = 50;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.multiply(firstNumber, secondNumber);
+        if (realResult == expectedResult) {
+            System.out.println("Multiply test = OK");
+        } else {
+            System.out.println("Multiply test = FAIL");
+        }
+    }
+
+    public void divisionTest() {
+        int firstNumber = 10;
+        int secondNumber = 5;
+        int expectedResult = 2;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.division(firstNumber, secondNumber);
+        if (realResult == expectedResult) {
+            System.out.println("Division test = OK");
+        } else {
+            System.out.println("Division test = FAIL");
+        }
+    }
+
+    public void isFirstEvenTest() {
+        int firstNumber = 10;
+        boolean expectedResult = true;
+        Calculator calculator = new Calculator();
+        boolean realResult = calculator.isFirstEven(10);
+        if (firstNumber % 2 == 0) {
+            System.out.println("Firs Even test = OK");
+        } else {
+            System.out.println("First Even test = FAIL");
+        }
+    }
+
+    public void isSecondEvenTest() {
+        int secondNumber = 5;
+        boolean expectedResult = false;
+        Calculator calculator = new Calculator();
+        boolean realResult = calculator.isSecondEven(5);
+        if (secondNumber % 2 == 0) {
+            System.out.println("Second Odd test = FAIL");
+        } else {
+            System.out.println("Second Odd test = OK");
+        }
+    }
+
+    public void firstMaxOfTwoNumbersTest() {
+        int firstNumber = 10;
+        int secondNumber = 5;
+        int expectedResult = 10;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.maxOfTwoNumbers(firstNumber, secondNumber);
+        if (realResult == expectedResult) {
+            System.out.println("First Number Bigger test = OK");
+        } else {
+            System.out.println("First Number Bigger test = FAIL");
+        }
+    }
+
+    public void secondMaxOfTwoNumbersTest() {
+        int firstNumber = 10;
+        int secondNumber = 25;
+        int expectedResult = 25;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.maxOfTwoNumbers(firstNumber, secondNumber);
+        if (realResult == expectedResult) {
+            System.out.println("Second Number Bigger test = OK");
+        } else {
+            System.out.println("Second Number Bigger test = FAIL");
+        }
+    }
+
+    public void thirdMaxOfTwoNumbersTest() {
+        int firstNumber = 10;
+        int secondNumber = 10;
+        int expectedResult = 10;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.maxOfTwoNumbers(firstNumber, secondNumber);
+        if (realResult == expectedResult) {
+            System.out.println("All numbers are equals test = OK");
+        } else {
+            System.out.println("All numbers are equals test = FAIL");
+        }
+    }
+
+    public void firstMaxOfThreeNumbersTest() {
+        int firstNumber = 10;
+        int secondNumber = 0;
+        int thirdNumber = 3;
+        int expectedResult = 10;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.maxOfThreeNumbers(firstNumber, secondNumber, thirdNumber);
+        if (realResult == expectedResult) {
+            System.out.println("First number is bigger test = OK");
+        } else {
+            System.out.println("First number is bigger test = FAIL");
+        }
+    }
+
+    public void secondMaxOfThreeNumbersTest() {
+        int firstNumber = 0;
+        int secondNumber = 10;
+        int thirdNumber = 3;
+        int expectedResult = 10;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.maxOfThreeNumbers(firstNumber, secondNumber, thirdNumber);
+        if (realResult == expectedResult) {
+            System.out.println("Second number is bigger test = OK");
+        } else {
+            System.out.println("Second number is bigger test = FAIL");
+        }
+    }
+
+    public void thirdMaxOfThreeNumbersTest() {
+        int firstNumber = 1;
+        int secondNumber = 1;
+        int thirdNumber = 1;
+        int expectedResult = 1;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.maxOfThreeNumbers(firstNumber, secondNumber, thirdNumber);
+        if (realResult == expectedResult) {
+            System.out.println("All equal numbers test = OK");
+        } else {
+            System.out.println("All equal numbers test = FAIL");
+        }
+    }
+
+    public void fourthMaxOfThreeNumbersTest() {
+        int firstNumber = 14;
+        int secondNumber = 14;
+        int thirdNumber = 1;
+        int expectedResult = 14;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.maxOfThreeNumbers(firstNumber, secondNumber, thirdNumber);
+        if (realResult == expectedResult) {
+            System.out.println("First numbers equals test = OK");
+        } else {
+            System.out.println("First numbers equals test = FAIL");
+        }
+    }
+
+    public void fifthMaxOfThreeNumbersTest() {
+        int firstNumber = 1;
+        int secondNumber = 14;
+        int thirdNumber = 14;
+        int expectedResult = 14;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.maxOfThreeNumbers(firstNumber, secondNumber, thirdNumber);
+        if (realResult == expectedResult) {
+            System.out.println("Second and Third numbers equals test = OK");
+        } else {
+            System.out.println("Second and Third numbers equals test = FAIL");
+        }
+    }
+
+    public void sixthMaxOfThreeNumbersTest() {
+        int firstNumber = 14;
+        int secondNumber = 1;
+        int thirdNumber = 14;
+        int expectedResult = 14;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.maxOfThreeNumbers(firstNumber, secondNumber, thirdNumber);
+        if (realResult == expectedResult) {
+            System.out.println("First and Third numbers equals test = OK");
+        } else {
+            System.out.println("First and Third numbers equals test = FAIL");
+        }
+    }
+}
+
