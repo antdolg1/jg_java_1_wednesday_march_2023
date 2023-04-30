78e45c13d5358b66d9d1869cfbcb8e3b75b23983
Roman <haron636@gmail.com>
2023-04-30 23:38:25 +0300

    create folders and test file

---
+++ src/main/java/students/romans_golubevs/lesson_1/test.java
@@ -0,0 +1,12 @@
+package students.romans_golubevs.lesson_1;
+
+public class test {
+
+    public static void main(String [] args) {
+        System.out.println("...");
+        System.out.println("...");
+        System.out.println("test1");
+        System.out.println("...");
+        System.out.println("...");
+  }
+}
