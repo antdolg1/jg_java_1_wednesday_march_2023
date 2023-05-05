bd7a96934f8643d4d669c7954a9c827cec074f39
DianaTr-01 <dia.t1912@gmail.com>
2023-05-05 20:26:02 +0300

    Lesson 2 homework completed and ready for review

--- src/main/java/students/diana_trekancenoka/lesson_2/level_6_middle/task_23/AttentionPlease.java
+++
@@ -1,8 +0,0 @@
-package students.diana_trekancenoka.lesson_2.level_6_middle.task_23;
-
-public class AttentionPlease {
-
-    public static void main(String[] args) {
-        System.out.println("Code outside the method will not compile!");
-    }
-}
--- src/main/java/students/diana_trekancenoka/lesson_2/level_6_middle/task_24/NoSleepNoFocus.java
+++
@@ -1,8 +0,0 @@
-package students.diana_trekancenoka.lesson_2.level_6_middle.task_24;
-
-public class NoSleepNoFocus {
-    public static void main(String[] args) {
-        System.out.println("I wanna coffee");
-        System.out.println("So bad");
-    }
-}
