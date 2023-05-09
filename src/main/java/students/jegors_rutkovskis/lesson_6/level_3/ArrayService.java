747145733a36562761007fbc1a78b84edf11578f
Jegors Rutkovskis <domerungus@gmail.com>
2023-05-09 21:58:02 +0300

    Small modification to lesson_6.level_3

--- src/main/java/students/jegors_rutkovskis/lesson_6/level_3/ArrayService.java
+++ src/main/java/students/jegors_rutkovskis/lesson_6/level_3/ArrayService.java
@@ -53,9 +53,7 @@ class ArrayService {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 1; j < arr.length; j++) {
                 if (arr[j] < arr[j - 1]) {
-                    int temp = arr[j - 1];
-                    arr[j - 1] = arr[j];
-                    arr[j] = temp;
+                    swap(arr, j - 1, j);
                 }
             }
         }
@@ -73,5 +71,10 @@ class ArrayService {
             }
         }
         return true;
+    }
+    void swap(int[] arr, int leftIndex, int rightIndex) {
+        int temp = arr[leftIndex];
+        arr[leftIndex] = arr[rightIndex];
+        arr[rightIndex] = temp;
     }
 }
