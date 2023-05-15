3441e607b2d602b71e10db13e01ece2448bb0fdc
vladislav_antoniv <vladislavs.antonivs@edu.rtu.lv>
2023-05-15 21:09:56 +0300

    fixed lesson2 homework

--- src/main/java/students/vladislav_antoniv/lesson_2/level_1/SimpleCalculatorOfTwoDoubleNumbers.java
+++ src/main/java/students/vladislav_antoniv/lesson_2/level_1/SimpleCalculatorOfTwoDoubleNumbers.java
@@ -1,8 +1,9 @@
 package students.vladislav_antoniv.lesson_2.level_1;
+
 import java.util.Scanner;
 
 public class SimpleCalculatorOfTwoDoubleNumbers {
-    public static void main(String[]args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter first number");
         double firstNumber = sc.nextDouble();
