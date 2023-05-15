b2e5df4063c4a0209e9a8571da866961cd1f5f81
vladislav_antoniv <vladislavs.antonivs@edu.rtu.lv>
2023-05-15 20:40:45 +0300

    Added_lesson5_homework_level1&level2

---
+++ src/main/java/students/vladislav_antoniv/lesson_5/level_2/ArrayOfThreeRandomPlusTwoEach.java
@@ -0,0 +1,22 @@
+package students.vladislav_antoniv.lesson_5.level_2;
+
+import java.util.Random;
+
+public class ArrayOfThreeRandomPlusTwoEach {
+    public static void main(String[] args) {
+        int numbers[] = new int[3];
+        Random random = new Random();
+        for (int i = 0; i < 3; i++) {
+            numbers[i] = random.nextInt();
+        }
+        for (int i = 0; i < 3; i++) {
+            System.out.println(numbers[i]);
+        }
+        for (int i = 0; i < 3; i++) {
+            numbers[i] += 2;
+        }
+        for (int i = 0; i < 3; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
