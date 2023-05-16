43b784f01de9bba24c86658bbccd724b02f4c742
SlavaJavaGuru <slv53@inbox.lv>
2023-05-16 22:24:18 +0300

    Here is Homeworks of Lesson_6 for Level_1 and Level2, please check it..

---
+++ src/main/java/students/slava_khl/lesson_6/level_2/DayOfTheWeekDemo.java
@@ -0,0 +1,11 @@
+package students.slava_khl.lesson_6.level_2;
+
+class DayOfTheWeekDemo {
+
+    public static void main(String[] args) {
+        DayOfTheWeekDetector detector = new DayOfTheWeekDetector();
+        int dayNumber = detector.getDayNumberFromUser();
+        String dayOfTheWeek = detector.findDayOfTheWeek(dayNumber);
+        System.out.println(dayOfTheWeek);
+    }
+}
