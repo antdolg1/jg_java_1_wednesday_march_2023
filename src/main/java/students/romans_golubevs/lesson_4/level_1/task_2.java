7e5959f2d5fb4fc22d757c10e68e513f01c427bb
romansgolubevs <haron636@gmail.com>
2023-06-09 01:20:44 +0300

    finished lesson 4 level 1

---
+++ src/main/java/students/romans_golubevs/lesson_4/level_1/task_2.java
@@ -0,0 +1,26 @@
+package students.romans_golubevs.lesson_4.level_1;
+
+import java.util.Scanner;
+
+public class task_2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number:");
+
+        int number = sc.nextInt();
+
+        if (number > 0) {
+            System.out.println("Your number " + number + " is positive!");
+        }
+
+        if (number < 0) {
+            System.out.println("Your number " + number + " is negative!");
+        }
+
+        if (number == 0) {
+            System.out.println("Your number is: " + number);
+        }
+    }
+}
